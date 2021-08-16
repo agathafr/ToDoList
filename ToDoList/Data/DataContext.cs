0b7962adceb81cc9da23e9a202024f075ff1a23a
Agatha França <agathafranca@outlook.com>
2021-08-16 15:43:59 -0300

    adding the constructor and specifying for the entity which property it will map

--- ToDoList/Data/DataContext.cs
+++ ToDoList/Data/DataContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using ToDoList.Models;
 
 namespace ToDoList.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext() : base("ToDoListConn")
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
     }
 }
