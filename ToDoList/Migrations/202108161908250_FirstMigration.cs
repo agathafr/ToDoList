9aecad5a489cb2d607ea73d632fae7d85585574e
Agatha França <agathafranca@outlook.com>
2021-08-16 16:12:31 -0300

    migrating data from the model to the database

---
+++ ToDoList/Migrations/202108161908250_FirstMigration.cs
@@ -0,0 +1,29 @@
+﻿namespace ToDoList.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FirstMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tarefas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Descricao = c.String(),
+                        Data = c.DateTime(nullable: false),
+                        Executada = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Tarefas");
+        }
+    }
+}
